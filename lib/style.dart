d38cbe1e6f126641b5b48e116d96e3b00a0063b5
eomchanu <djacksdn1@icloud.com>
2023-10-16 23:13:24 +0900

    create style.dart: set default colors

---
+++ lib/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColor extends Color {
+  AppColor(super.value);
+
+  static const backgroudColor = Color(0xFFF2F2F2);
+  static const navigationBarColor = Color(0xE5DED7D2);
+  static const color1 = Color(0xFFC92A5A);
+  static const color2 = Color(0xFFB425A9);
+  static const color3 = Color(0xFF7637F4);
+  static const color4 = Color(0xFF1CD14F);
+  static const color5 = Color(0xFF3897F0);
+  static const color6 = Color(0xFF996438);
+  static const color7 = Color(0xFFED772F);
+  static const color8 = Color(0xFF432323);
+  static const color9 = Color(0xFFFDCB5C);
+  static const color10 = Color(0xFFED858E);
+
+}
