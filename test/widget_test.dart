65f8a311dd6b3d1ce4e52f6c898250c4c629fa8e
frankwon11 <frankwon11@gmail.com>
2023-10-17 22:33:44 +0900

    modify test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:musiq_front/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MUSIQ());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
