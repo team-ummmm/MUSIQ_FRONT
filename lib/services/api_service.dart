48e7d2390d3f34571baa52d9c618e527ba49d055
frankwon11 <frankwon11@gmail.com>
2023-10-27 17:41:32 +0900

    create models: music model, question_model

---
+++ lib/services/api_service.dart
@@ -0,0 +1,9 @@
+import 'dart:convert';
+
+import 'package:musiq_front/models/question_model.dart';
+
+class ApiService {
+  static const String baseUrl = 'https://seungdev.com';
+
+  // static Future<List<QuestionModel>> getThreeQuestions()
+}
