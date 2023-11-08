3136342041bd253b915e388b4fd65a3d4c777f57
frankwon11 <frankwon11@gmail.com>
2023-11-08 16:34:37 +0900

    implement search_music_controller and model

--- lib/models/search_music_model.dart
+++ lib/models/search_music_model.dart
@@ -1,16 +1,7 @@
 class SearchMusicModel {
-  final String error;
-  final List<Data> data;
-
-  SearchMusicModel.fromJson(Map<String, dynamic> json)
-      : error = json['error'],
-        data = json['data'].map((i) => Data.fromJson(i)).toList();
-}
-
-class Data {
   final String music_id, music_name, artist_name, cover_url;
 
-  Data.fromJson(Map<String, dynamic> json)
+  SearchMusicModel.fromJson(Map<String, dynamic> json)
       : music_id = json['music_id'],
         music_name = json['music_name'],
         artist_name = json['artist_name'],
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
+import 'dart:html';
 
 import 'package:http/http.dart' as http;
 import 'package:musiq_front/models/question_model.dart';
+import 'package:musiq_front/models/search_music_model.dart';
 
 class ApiService {
   // TODO: 수정 예정
-  static const String baseUrl = 'https://seungdev.com';
-  // TODO: restAPI 따라 뒤이은 도메인 추가 예정
+  static const String baseUrl =
+      'http://musiq-api.ap-northeast-2.elasticbeanstalk.com/';
+  static const String searchSong = 'search?search_text=';
 
   /// 사용자 질문들 받기. 각 질문의 대답들은 따로 받음.
   static Future<List<QuestionModel>> getQuestions() async {
@@ -20,6 +23,25 @@ class ApiService {
         questionInstances.add(QuestionModel.fromJson(question));
       }
       return questionInstances;
+    }
+    throw Error();
+  }
+
+  /// 검색 결과 곡들 받기.
+  /// 200: 검색된 곡들 리스트로 바로 옴.
+  /// 400: 에러
+  static Future<List<SearchMusicModel>> getSearchMusics(
+      String searchText) async {
+    List<SearchMusicModel> searchMusicInstances = [];
+    final url = Uri.parse("$baseUrl$searchSong$searchText");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> searchMusics = jsonDecode(response.body);
+      for (var searchMusic in searchMusics) {
+        searchMusicInstances.add(SearchMusicModel.fromJson(searchMusic));
+      }
+      return searchMusicInstances;
     }
     throw Error();
   }
